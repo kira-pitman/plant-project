ab180072d09396d4a8b7b35f1334d7a869503963
Kira Pitman <kirapitman@gmail.com>
2023-09-04 13:30:38 +1200

    create server path for main page

--- server/server.ts
+++ server/server.ts
@@ -1,9 +1,12 @@
 import express from 'express'
 import * as Path from 'node:path'
 
+import plantsRoute from './routes/plants-routes.ts'
+
 const server = express()
 
 server.use(express.json())
+server.use('api/v1/plants', plantsRoute)
 
 if (process.env.NODE_ENV === 'production') {
   server.use(express.static(Path.resolve('public')))
