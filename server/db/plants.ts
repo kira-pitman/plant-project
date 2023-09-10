b819fc21c699b7eedff65e282bb735a32f72290b
Kira Pitman <kirapitman@gmail.com>
2023-09-11 11:05:29 +1200

    write addPlant function

--- server/db/plants.ts
+++ server/db/plants.ts
@@ -1,4 +1,4 @@
-import { Plant } from '../../models/plants.ts'
+import { Plant, newPlant } from '../../models/plants.ts'
 import db from './connection.ts'
 
 export async function getAllPlants(): Promise<Plant[]> {
@@ -11,4 +11,8 @@ export async function getPlantById(id: number){
 
 export async function deletePlant(id: number) {
   return await db('plants').where({id}).delete()
+}
+
+export async function addPlant(plant: newPlant) {
+  return db('plants').insert({plant}).returning(['id', 'name', 'height', 'location', 'facts', 'image'])
 }
