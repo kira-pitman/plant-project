bda166569147bf3a43085188e006e4b308f8f38e
Kira Pitman <kirapitman@gmail.com>
2023-09-10 15:48:02 +1200

    start add plant mutation

--- client/components/PlantList.tsx
+++ client/components/PlantList.tsx
@@ -4,8 +4,6 @@ import { Plant, newPlant } from '../../models/plants'
 import { Link } from 'react-router-dom'
 import { useState } from 'react'
 
-
-
 //useQuery
 //also want to return plant list by mapping over everything perhaps?
 // then add link to click to {'${plant.id}/${plant.name}'}
@@ -17,8 +15,24 @@ export default function PlantList() {
     isLoading,
   } = useQuery(['plants'], fetchAllPlants)
   console.log(plantList)
-  
 
+  // want to add an add plant form? with a save button
+  const [editing, setEditing] = useState(false)
+  const [text, setText] = useState(plantList.name)
+  const queryClient = useQueryClient()
+
+  const addPlantMutation = useMutation(addPlant, {
+    onSuccess: async () => {
+      queryClient.invalidateQueries(['plants'])
+    },
+  })
+
+  const handleAddClick = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    addPlantMutation.mutate({
+      //idk what to mutate in here
+    })
+  }
 
   if (isError) {
     return <p>Whoops! Time to get some plants, as yours do not live here!</p>
@@ -40,6 +54,8 @@ export default function PlantList() {
           )
         })}
       </ul>
+
+      {/* add plant form? */}
     </>
   )
 }
