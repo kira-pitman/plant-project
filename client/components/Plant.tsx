f8f02c6781c134691a7c17fa5ca394a6b48ee538
Kira Pitman <kirapitman@gmail.com>
2023-12-01 15:33:11 +1300

    relabel heading

--- client/components/Plant.tsx
+++ client/components/Plant.tsx
@@ -44,15 +44,21 @@ export default function PlantInfo() {
 
   return (
     <>
-      <h3>{plant?.name}</h3>
-      <img src={plant?.image} alt={`${plant.name}`} className="homeImage" />
+    <div className='PlantPage'>
+    <div className='nameImage'>
+      <h2>{plant?.name}</h2>
+      <img src={plant?.image} alt={`${plant.name}`} className="individualImage" />
+      </div>
+      <div className='plantInfo'>
       <h4>Height</h4>
       <p> {plant?.height}</p>
       <h4>Location</h4>
       <p> {plant?.location}</p>
       <h4>Facts</h4>
       <p> {plant?.facts}</p>
-      <br />
+      </div>
+      </div>
+      <div className='buttons'>
       <button
         className="deleteButton"
         onClick={(e) => handleDeleteClick(e, plant.id)}
@@ -62,6 +68,7 @@ export default function PlantInfo() {
       <Link to={`/${id}/edit`} className="toEdit">
         <button className="editButton">Edit</button>
       </Link>
+      </div>
     </>
   )
 }
